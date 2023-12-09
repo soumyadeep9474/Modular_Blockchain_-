f4e2406b17f05f5b40fdbca28c84931af4f3b26b
Soumya <soumyadeep1729@gmail.com>
2023-12-09 15:10:07 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/anthdm/projectx/core"
+	"github.com/anthdm/projectx/crypto"
+	"github.com/anthdm/projectx/network"
+	"github.com/anthdm/projectx/types"
+	"github.com/anthdm/projectx/util"
+)
+
+func main() {
+	validatorPrivKey := crypto.GeneratePrivateKey()
+	localNode := makeServer("LOCAL_NODE", &validatorPrivKey, ":3000", []string{":4000"}, ":9000")
+	go localNode.Start()
+
+	remoteNode := makeServer("REMOTE_NODE", nil, ":4000", []string{":5000"}, "")
+	go remoteNode.Start()
+
+	remoteNodeB := makeServer("REMOTE_NODE_B", nil, ":5000", nil, "")
+	go remoteNodeB.Start()
+
+	go func() {
+		time.Sleep(11 * time.Second)
+
+		lateNode := makeServer("LATE_NODE", nil, ":6000", []string{":4000"}, "")
+		go lateNode.Start()
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	select {}
+}
+
+func sendTransaction(privKey crypto.PrivateKey) error {
+	toPrivKey := crypto.GeneratePrivateKey()
+
+	tx := core.NewTransaction(nil)
+	tx.To = toPrivKey.PublicKey()
+	tx.Value = 666
+
+	if err := tx.Sign(privKey); err != nil {
+		return err
+	}
+
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/tx", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+
+	return err
+}
+
+func makeServer(id string, pk *crypto.PrivateKey, addr string, seedNodes []string, apiListenAddr string) *network.Server {
+	opts := network.ServerOpts{
+		APIListenAddr: apiListenAddr,
+		SeedNodes:     seedNodes,
+		ListenAddr:    addr,
+		PrivateKey:    pk,
+		ID:            id,
+	}
+
+	s, err := network.NewServer(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return s
+}
+
+func createCollectionTx(privKey crypto.PrivateKey) types.Hash {
+	tx := core.NewTransaction(nil)
+	tx.TxInner = core.CollectionTx{
+		Fee:      200,
+		MetaData: []byte("chicken and egg collection!"),
+	}
+	tx.Sign(privKey)
+
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/tx", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	return tx.Hash(core.TxHasher{})
+}
+
+func nftMinter(privKey crypto.PrivateKey, collection types.Hash) {
+	metaData := map[string]any{
+		"power":  8,
+		"health": 100,
+		"color":  "green",
+		"rare":   "yes",
+	}
+
+	metaBuf := new(bytes.Buffer)
+	if err := json.NewEncoder(metaBuf).Encode(metaData); err != nil {
+		panic(err)
+	}
+
+	tx := core.NewTransaction(nil)
+	tx.TxInner = core.MintTx{
+		Fee:             200,
+		NFT:             util.RandomHash(),
+		MetaData:        metaBuf.Bytes(),
+		Collection:      collection,
+		CollectionOwner: privKey.PublicKey(),
+	}
+	tx.Sign(privKey)
+
+	buf := &bytes.Buffer{}
+	if err := tx.Encode(core.NewGobTxEncoder(buf)); err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:9000/tx", buf)
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+}
